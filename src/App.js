cdbc73186129a6b9dc0dcd31895e579820541c34
Olof Larsson <olof@sylt.nu>
2020-05-16 22:40:38 +0200

    Make it work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import './App.css';
 
+const getDiceValue = () => {
+  return Math.ceil(Math.random() * 10)
+}
+
+const getDiceSuccesses = (again) => {
+  let successes = 0
+  const value = getDiceValue()
+  if (value >= 8) {
+    successes += 1
+  }
+  if (value >= again) {
+    successes += getDiceSuccesses(again)
+  }
+  return successes
+}
+
+const getDiceSuccessesMultiple = (dices, again) => {
+  let successes = 0
+  for (let index = 0; index < dices; index++) {
+    successes += getDiceSuccesses(again)
+  }
+  return successes
+}
+
 class App extends React.Component {
   state = {
     dices: 4,
@@ -8,14 +32,40 @@ class App extends React.Component {
     successes: 0
   }
 
+  handleDicesChanged = (event) => {
+    const dices = event.target.value
+    this.setState({ dices })
+  }
+
+  handleAgainChanged = (event) => {
+    const again = event.target.value
+    this.setState({ again })
+  }
+
+  handleRoll = () => {
+    const { dices, again } = this.state
+    const successes = getDiceSuccessesMultiple(dices, again)
+    this.setState({ successes })
+  }
+
   render() {
-    const {dices, again, successes} = this.state
+    const { dices, again, successes } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <p>Dices: {dices}</p>
-          <p>Again: {again}</p>
-          <p>Successes: {successes}</p>
+          <div>
+            <input type="number" value={dices} onChange={this.handleDicesChanged} />
+          </div>
+
+          <div>
+            <input type="number" value={again} onChange={this.handleAgainChanged} />
+          </div>
+
+          <div>
+            <button type="button" onClick={this.handleRoll}>Roll</button>
+          </div>
+
+          <p>{successes} Successes</p>
         </header>
       </div>
     );
