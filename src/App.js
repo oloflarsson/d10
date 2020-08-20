39aeaae53005ac40fe32a7691493652bf36c3272
Olof Larsson <olof@sylt.nu>
2020-08-20 21:59:40 +0200

    rote action quality

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import LocalStorageAccessor from './LocalStorageAccessor';
 
 const STARTING_DICES = 4
 const STARTING_AGAIN = 10
+const STARTING_ROTE = false
 const STARTING_EXPECTED = DiceUtil.getExpected(STARTING_DICES, STARTING_AGAIN)
 const STARTING_CHANGE = DiceUtil.getChance(STARTING_DICES)
 const STARTING_ROLL_ENABLED = true
 const STARTING_WILLPOWER_ENABLED = true
 const STARTING_SUCCESSES = 0
 const STARTING_WILLPOWER_SUCCESSES = 0
-const STORAGE = new LocalStorageAccessor('storage', {
+const STORAGE = new LocalStorageAccessor('storagev2', {
   dices: STARTING_DICES,
   again: STARTING_AGAIN,
+  rote: STARTING_ROTE,
   expected: STARTING_EXPECTED,
   chance: STARTING_CHANGE,
   rollEnabled: STARTING_ROLL_ENABLED,
@@ -30,10 +32,10 @@ class App extends React.Component {
   }
 
   handleDicesChanged = (event) => {
-    const { again } = this.state
+    const { again, rote } = this.state
     const dices = event.target.value
-    const expected = DiceUtil.getExpected(dices, again)
-    const chance = DiceUtil.getChance(dices)
+    const expected = DiceUtil.getExpected(dices, again, rote)
+    const chance = DiceUtil.getChance(dices, rote)
     this.setState({
       dices,
       expected,
@@ -46,12 +48,28 @@ class App extends React.Component {
   }
 
   handleAgainChanged = (event) => {
-    const { dices } = this.state
+    const { dices, rote } = this.state
     const again = event.target.value
-    const expected = DiceUtil.getExpected(dices, again)
-    const chance = DiceUtil.getChance(dices)
+    const expected = DiceUtil.getExpected(dices, again, rote)
+    const chance = DiceUtil.getChance(dices, rote)
     this.setState({
       again,
+      expected,
+      chance,
+      rollEnabled: STARTING_ROLL_ENABLED,
+      willpowerEnabled: STARTING_WILLPOWER_ENABLED,
+      successes: STARTING_SUCCESSES,
+      willpowerSuccesses: STARTING_WILLPOWER_SUCCESSES,
+    }, this.saveState)
+  }
+
+  handleRoteChanged = (event) => {
+    const { again, dices } = this.state
+    const rote = event.target.checked
+    const expected = DiceUtil.getExpected(dices, again, rote)
+    const chance = DiceUtil.getChance(dices, rote)
+    this.setState({
+      rote,
       expected,
       chance,
       rollEnabled: STARTING_ROLL_ENABLED,
@@ -71,8 +89,8 @@ class App extends React.Component {
   }
 
   handleRoll = () => {
-    const { dices, again, successes } = this.state
-    const additionalSuccesses = DiceUtil.getSuccesses(dices, again)
+    const { dices, again, rote, successes } = this.state
+    const additionalSuccesses = DiceUtil.getSuccesses(dices, again, rote)
     const targetSuccesses = successes + additionalSuccesses
     this.setState({
       rollEnabled: false,
@@ -81,9 +99,9 @@ class App extends React.Component {
   }
 
   handleWillpower = () => {
-    const { again, successes } = this.state
+    const { again, rote, successes } = this.state
     const dices = 3
-    const additionalSuccesses = DiceUtil.getSuccesses(dices, again)
+    const additionalSuccesses = DiceUtil.getSuccesses(dices, again, rote)
     const targetSuccesses = successes + additionalSuccesses
     this.setState({
       willpowerEnabled: false,
@@ -93,13 +111,14 @@ class App extends React.Component {
   }
 
   render() {
-    const { dices, again, expected, chance, rollEnabled, willpowerEnabled, successes, willpowerSuccesses } = this.state
+    const { dices, again, rote, expected, chance, rollEnabled, willpowerEnabled, successes, willpowerSuccesses } = this.state
 
     const chanceDescription = '' + Math.round(chance * 100) + '%'
     const expectedDescription = '' + Math.round(expected * 100) / 100
     
     const tooltipDices = 'The amount of dices to be rolled.'
     const tooltipAgain = 'Roll one more dice if the dice shows this value and above.'
+    const tooltipRote = 'Reroll all failed dice on the initial roll while not rerolling the results of that reroll, or the results of any 8, 9, or 10-again rerolls.'
     const tooltipChance = `The chance to get at least 1 success is ${chanceDescription} when rolling ${dices} dices.`
     const tooltipExpected = `The average amount of successes is ${expectedDescription} when rolling ${dices} dices with ${again}-again.`
 
@@ -120,7 +139,15 @@ class App extends React.Component {
                 Again:
               </div>
               <div className="value">
-                <input className="input" type="number" min="2" max="11" value={again} onChange={this.handleAgainChanged} />
+                <input className="input" type="number" min="8" max="11" value={again} onChange={this.handleAgainChanged} />
+              </div>
+            </div>
+            <div className="entry">
+              <div className="key" title={tooltipRote}>
+                Rote:
+              </div>
+              <div className="value">
+                <input className="input checkbox" type="checkbox" checked={rote} onChange={this.handleRoteChanged} />
               </div>
             </div>
           </div>
--- src/DiceUtil.js
+++ src/DiceUtil.js
@@ -2,26 +2,35 @@ const getValue = () => {
   return Math.ceil(Math.random() * 10)
 }
 
-const getSuccessesSingle = (again) => {
-  let successes = 0
+const getSuccessesSingle = (again, rote) => {
   const value = getValue()
-  if (value >= 8) {
-    successes += 1
+
+  if (value < 8) {
+    if (rote) {
+      return getSuccessesSingle(again, false)
+    } else {
+      return 0
+    }
   }
+
+  let successes = 1
+  
   if (value >= again) {
-    successes += getSuccessesSingle(again)
+    successes += getSuccessesSingle(again, false)
   }
+
   return successes
 }
 
-const getSuccesses = (dices, again) => {
+const getSuccesses = (dices, again, rote) => {
   let successes = 0
   for (let index = 0; index < dices; index++) {
-    successes += getSuccessesSingle(again)
+    successes += getSuccessesSingle(again, rote)
   }
   return successes
 }
 
+/*
 const getExpectedSingleSampledUnmemoized = (again, samples) => {
   let sum = 0
   for (let index = 0; index < samples; index++) {
@@ -30,10 +39,28 @@ const getExpectedSingleSampledUnmemoized = (again, samples) => {
   return sum / samples
 }
 window.test1 = getExpectedSingleSampledUnmemoized
+*/
 
-const getExpectedSingle = (again) => {
-  if (again <= 1) {
-    throw new Error('again must be larger than or equal to 2')
+const getExpectedSingle = (again, rote) => {
+  if (again < 8) {
+    throw new Error('again must be larger than or equal to 8')
+  }
+
+  if (rote) {
+    // There is three dice "areas"
+    // The (failure) reroll area, which has the expected value of a non rote dice
+    // The (success) again area, which has the value of one + non rote dice
+    // The (success) only area, which has the value of one
+
+    const rerollArea = 7
+    const againArea = 10 - again + 1
+    const onlyArea = 10 - rerollArea - againArea
+
+    const rerollValue = getExpectedSingle(again, false)
+    const againValue = 1 + getExpectedSingle(again, false)
+    const onlyValue = 1
+
+    return rerollValue * rerollArea / 10 + againValue * againArea / 10 + onlyValue * onlyArea / 10
   }
 
   if (again >= 11) {
@@ -43,12 +70,17 @@ const getExpectedSingle = (again) => {
   return 3 / (again - 1)
 }
 
-const getExpected = (dices, again) => {
-  const expectedSingle = getExpectedSingle(again)
+const getExpected = (dices, again, rote) => {
+  const expectedSingle = getExpectedSingle(again, rote)
   return expectedSingle * dices
 }
 
-const getChance = (dices) => {
+const getChance = (dices, rote) => {
+  if (rote) {
+    // You need to fail with all dices, then roll them all again and fail again.
+    // So in practice you need to fail with twice the dices
+    dices *= 2;
+  }
   return 1 - Math.pow(0.7, dices)
 }
 
